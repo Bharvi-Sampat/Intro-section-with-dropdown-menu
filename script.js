e150b75089db0566fe26b368d80dffe7a4858338
Bharvi-Sampat <bharvi.sampat@gmail.com>
2022-07-14 01:12:27 +0530

    Overlay and hamburger nav edited

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hamburger = document.querySelector(".menu-btn__burger");
 const nav = document.querySelector(".nav");
 const menuNav = document.querySelector(".menu-nav");
 const navItems = document.querySelectorAll(".menu-nav__item");
-
+const overlay = document.querySelector(".overlay");
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggelMenu);
@@ -13,13 +13,14 @@ function toggelMenu() {
     hamburger.classList.add("open");
     nav.classList.add("open");
     menuNav.classList.add("open");
+    overlay.classList.add("black-overlay");
     navItems.forEach((item) => item.classList.add("open"));
-
     showMenu = true;
   } else {
     hamburger.classList.remove("open");
     nav.classList.remove("open");
     menuNav.classList.remove("open");
+    overlay.classList.remove("black-overlay");
     navItems.forEach((item) => item.classList.remove("open"));
 
     showMenu = false;
